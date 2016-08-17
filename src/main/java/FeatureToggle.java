935ff5fc1378a56efbac0b05b67515d1e7d897ef
Alejosd <alejosd5@gmail.com>
2016-08-17 01:25:51 -0300

    implementación feature toggle

---
+++ src/main/java/FeatureToggle.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+
+import com.launchdarkly.client.LDClient;
+import com.launchdarkly.client.LDUser;
+
+public class FeatureToggle {
+	
+	private Message message;
+	
+	public FeatureToggle(Message message){
+		this.message=message;
+	}
+	
+	  public Message getMessage() {
+		return message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
+	}
+
+	public Message featureToggle() throws IOException{
+	
+		Message message = new Message("");
+		
+		   LDClient client = new LDClient("sdk-548dca6e-ae25-4a5f-861d-8f854a267956");
+	       LDUser user = new LDUser("alejosd5@gmail.com");
+	       		boolean showFeature = client.boolVariation("sd", user,false);
+
+	       		if (showFeature) {
+	       		  System.out.println("Showing your feature");
+	       		  message.setMessage("Java Spark Activado Feature Toggle");
+	       	      
+	       		} else {
+	       		  System.out.println("Not showing your feature");
+	       		message.setMessage("Java Spark Desactivado Feature Toggle");
+	       	
+	       		}
+	       		client.flush();
+	       		client.close();
+	       		
+	       		return message;
+	  }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,31 @@ import static spark.Spark.*;
 
 import static spark.Spark.get;
 
+import java.beans.FeatureDescriptor;
+import java.io.IOException;
+
+import org.eclipse.jetty.http.HttpStatus;
+
+import com.launchdarkly.client.LDClient;
+import com.launchdarkly.client.LDUser;
+
 public class Main {
+
 
   public static void main(String[] args) {
 
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
+   port(Integer.valueOf(System.getenv("PORT")));
+  
+   Message message = new Message("hola mundo");
+   FeatureToggle featureToggle = new FeatureToggle(message);
 
     get("/", "application/json", (request, response) -> {
-        return new Message("Hello Mundo Json");
+        return new Message(featureToggle.featureToggle().getMessage());
+        
+        
     }, new JsonTransformer());
+    
+   
    
   }
 
