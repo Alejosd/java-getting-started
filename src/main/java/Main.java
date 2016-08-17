0f336c2e490d2f37a3da9b661728160cb309a345
Alejosd <alejosd5@gmail.com>
2016-08-17 01:03:37 -0300

    cambiando path a /

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 
-import java.util.HashMap;
-import java.util.Map;
 import static spark.Spark.*;
-import spark.template.freemarker.FreeMarkerEngine;
-import spark.ModelAndView;
+
 import static spark.Spark.get;
 
 public class Main {
@@ -13,7 +10,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", "application/json", (request, response) -> {
+    get("/", "application/json", (request, response) -> {
         return new Message("Hello Mundo Json");
     }, new JsonTransformer());
    
